05f4c3ccc76632d055a13d4fef72b4c469d8ad2a
Frederick Brock <frederick.brock@gmail.com>
2016-04-28 03:49:07 -0700

    fixing array push

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ module.exports = (function() {
     pubnub.subscribe({
       channel: "AWG-global"
       ,message: function(message,e,ch){
+          winston.info("processing chat message: ", message);
+
           var r = /(\S*)@(\S+)/g
           if(r.match(message.content)){
             while(null != (value = r.exec(message.content))){
